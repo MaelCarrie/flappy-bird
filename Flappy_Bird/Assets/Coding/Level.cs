e42a459ec8ad024103790ff31e12b946c7cf36a8
MaelCarrie <mael.carrie@gmail.com>
2025-01-13 10:52:49 +0100

    Commit TP1

---
+++ Flappy_Bird/Assets/Coding/Level.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public float Speed = -5f;
+    public float deadZone = -20f;
+    Vector3 initPosition;
+
+    private void Start()
+    {
+        initPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Speed * Time.deltaTime, 0f);
+
+        if(transform.position.x < deadZone)
+        {
+            transform.position = initPosition;
+        }
+    }
+}
